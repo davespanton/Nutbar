987fb03c22756842bae9ce390d2603f21e10c513
Dave <d_spanton@hotmail.com>
2011-06-27 22:35:34 +0100

    Basic project skeleton

---
+++ src/test/java/com/davespanton/nutbar/NutbarActivityTest.java
@@ -0,0 +1,17 @@
+package com.davespanton.nutbar;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class NutbarActivityTest {
+
+	@Test
+	public void shouldFail() {
+		Assert.fail();
+	}
+}
