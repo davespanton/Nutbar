6d61579a0e4b58d58f5ea21da848498d26030b19
Dave <d_spanton@hotmail.com>
2011-10-20 23:18:41 +0100

    added start of sending sms on trip to sms alarm

--- src/main/java/com/davespanton/nutbar/alarms/SMSSendingAlarm.java
+++ src/main/java/com/davespanton/nutbar/alarms/SMSSendingAlarm.java
@@ -1,9 +1,16 @@
 package com.davespanton.nutbar.alarms;
 
+import android.telephony.SmsManager;
+
 public class SMSSendingAlarm {
 
+	private static final String destinationAddress = "";
+	private static final String bodyText = "Your thingy is on the move";
+	
+	private SmsManager smsManager = SmsManager.getDefault();
+	
 	public void tripAlarm() {
-		
+		smsManager.sendTextMessage(destinationAddress, null, bodyText, null, null);
 	}
 
 }
--- src/test/java/com/davespanton/nutbar/alarms/SMSSendingAlarmTest.java
+++ src/test/java/com/davespanton/nutbar/alarms/SMSSendingAlarmTest.java
@@ -1,30 +1,41 @@
 package com.davespanton.nutbar.alarms;
 
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import android.telephony.SmsManager;
+
+import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
+import com.xtremelabs.robolectric.shadows.ShadowSmsManager;
 
 @RunWith(RobolectricTestRunner.class)
 public class SMSSendingAlarmTest {
 
 	private SMSSendingAlarm sut;
 	
+	private ShadowSmsManager shadoSmsManager;
+	
 	@Before
 	public void setup() {
 		sut = new SMSSendingAlarm();
+		shadoSmsManager = Robolectric.shadowOf(SmsManager.getDefault());
 	}
 
 	@After
 	public void tearDown() {
 		sut = null;
+		shadoSmsManager = null;
 	}
 	
 	@Test
-	public void shouldSendSMSOrSomething() {
+	public void shouldSendSMSOnTrip() {
 		sut.tripAlarm();
-		//Assert.fail("doesn't send sms or something");
+		
+		Assert.assertNotNull(shadoSmsManager.getLastSentTextMessageParams());
 	}
 }
